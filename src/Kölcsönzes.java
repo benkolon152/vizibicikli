f9d6031de412fd81d18ffaf163de3040ad5c3788
Benkolon152 <v.benasz@gmail.com>
2025-09-19 07:59:27 +0200

    Solve task 2

---
+++ src/Kölcsönzes.java
@@ -0,0 +1,56 @@
+public class Kölcsönzes {
+    private String nev;
+    private char jarmu;
+    private int elvitelOra;
+    private int elvitelPerc;
+    private int visszahozOra;
+    private int visszahozPerc;
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public char getJarmu() {
+        return jarmu;
+    }
+
+    public void setJarmu(char jarmu) {
+        this.jarmu = jarmu;
+    }
+
+    public int getElvitelOra() {
+        return elvitelOra;
+    }
+
+    public void setElvitelOra(int elvitelOra) {
+        this.elvitelOra = elvitelOra;
+    }
+
+    public int getElvitelPerc() {
+        return elvitelPerc;
+    }
+
+    public void setElvitelPerc(int elvitelPerc) {
+        this.elvitelPerc = elvitelPerc;
+    }
+
+    public int getVisszahozOra() {
+        return visszahozOra;
+    }
+
+    public void setVisszahozOra(int visszahozOra) {
+        this.visszahozOra = visszahozOra;
+    }
+
+    public int getVisszahozPerc() {
+        return visszahozPerc;
+    }
+
+    public void setVisszahozPerc(int visszahozPerc) {
+        this.visszahozPerc = visszahozPerc;
+    }
+}
